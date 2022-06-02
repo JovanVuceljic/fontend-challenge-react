5143d49152324c31d7392edb744845c166399882
Jovan Vuceljic <jovanvuceljic@gmail.com>
2022-06-02 17:57:39 +0200

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Header from './components/Header';
+import Pagination from './components/Pagination';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders recipes overview text', () => {
+  render(<Header />);
+  const linkElement = screen.getByText(/recipes overview/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('filter input should be rendered', () => {
+  render(<Header />)
+  const filterInput = screen.getByPlaceholderText(/filter/i)
+  expect(filterInput).toBeInTheDocument()
+})
+
+test('filter input should be empty', () => {
+  render(<Header />)
+  const filterInput = screen.getByPlaceholderText(/filter/i)
+  expect(filterInput.value).toBe("")
+})
+
+
+test('pagination should render at least one button', () => {
+  render(<Pagination />)
+  const firstPageButton = screen.getAllByRole("button")
+  expect(firstPageButton.length).toBeGreaterThan(0)
+})
+
